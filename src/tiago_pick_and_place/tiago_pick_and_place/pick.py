42b2d18740bdd9a7cbd6dfd675fb00e9ac32c10e
Yoan Mollard <ymollard@users.noreply.github.com>
2025-02-09 20:45:50 +0100

    Make pick.py a regular rclpy node

--- src/tiago_pick_and_place/tiago_pick_and_place/pick.py
+++ src/tiago_pick_and_place/tiago_pick_and_place/pick.py
@@ -9,117 +9,105 @@ Code snippet commanding MoveIt planning and execution on Tiago from Python
 #   print(dir(plan_result)) # 'error_code', 'planner_id', 'planning_time', 'start_state', 'trajectory'
 """
 import rclpy
-from rclpy.logging import get_logger
+from rclpy.node import Node
 from moveit.core.robot_state import RobotState
 from moveit.planning import MoveItPy
 from geometry_msgs.msg import PoseStamped
 
 
+class MoveItExecutorNode(Node):
+    def __init__(self):
+        super().__init__('moveit_executor_node')
+        self.logger = self.get_logger()
+        self.logger.info("Initializing MoveItPy and executing motion...")
+        
+        # Initialize MoveItPy instance (already manages its own executor)
+        self.tiago = MoveItPy(node_name="moveit_py_pick")
+        self.robot_model = self.tiago.get_robot_model()
+        self.tiago_arm = self.tiago.get_planning_component("arm")
+        self.logger.info("MoveItPy connected!")
+
+        # Store joint values of open/close gripper positions
+        self.gripper_open_joints = {"left_finger": 0.04, "right_finger": 0.04}
+        self.gripper_closed_joints = {"left_finger": 0.0, "right_finger": 0.0}
+
+
+    
+    def plan1(self):
+        """
+        Plan 1 - set goal state with RobotState object (using its joint states)
+        """
+        # First go to the joint state with default values (zero angles)
+        robot_initial_state = RobotState(self.robot_model)
+        robot_state = RobotState(self.robot_model)
+        robot_state.set_to_default_values()
+        self.tiago_arm.set_start_state_to_current_state()
+        self.logger.info("Set goal state to the initialized robot state")
+        self.tiago_arm.set_goal_state(robot_state=robot_state)
+        plan_result = self.tiago_arm.plan()
+        if plan_result:
+            self.logger.info("Executing plan")
+            self.tiago.execute(plan_result.trajectory, controllers=[])
+        else:
+            self.logger.error("Planning failed")
+        
+        # Then go back to the initial state
+        self.tiago_arm.set_start_state_to_current_state()
+        self.tiago_arm.set_goal_state(robot_state=robot_initial_state)
+        plan_result = self.tiago_arm.plan()
+        if plan_result:
+            self.logger.warning("Moving arm to the joints goal (RobotState goal)...")
+            self.tiago.execute(plan_result.trajectory, controllers=[])
+    
+    def plan2(self):
+        """
+        Plan 2 - set cartesian goal for the gripper with PoseStamped message
+        """
+        self.tiago_arm.set_start_state_to_current_state()
+        pose_goal = PoseStamped()
+        pose_goal.header.frame_id = "base_link"
+        pose_goal.pose.position.x = 0.7
+        pose_goal.pose.position.y = 0.1
+        pose_goal.pose.position.z = 0.4
+        pose_goal.pose.orientation.x = 0.0
+        pose_goal.pose.orientation.y = 0.0
+        pose_goal.pose.orientation.z = 0.0
+        pose_goal.pose.orientation.w = 1.0
+        self.tiago_arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="gripper_grasping_frame")
+        plan_result = self.tiago_arm.plan()
+        if plan_result:
+            self.logger.warning("Moving arm to the cartesian goal...")
+            self.tiago.execute(plan_result.trajectory, controllers=[])
+    
+    def plan3(self):
+        """
+        Plan 3 - Open and close gripper
+        """
+        # First open fingers
+        tiago_gripper = self.tiago.get_planning_component("gripper")
+        gripper_open = RobotState(self.robot_model)
+        gripper_open.set_joint_group_positions("gripper", list(self.gripper_open_joints.values()))
+        tiago_gripper.set_goal_state(robot_state=gripper_open)
+        gripper_opening_result = tiago_gripper.plan()
+        if gripper_opening_result:
+            self.logger.warning("Opening Tiago's gripper...")
+            self.tiago.execute(gripper_opening_result.trajectory, controllers=[])
+        # Now close fingers
+        gripper_closed = RobotState(self.robot_model)
+        gripper_closed.set_joint_group_positions("gripper", list(self.gripper_closed_joints.values()))
+        tiago_gripper.set_goal_state(robot_state=gripper_closed)
+        gripper_closing_result = tiago_gripper.plan()
+        if gripper_closing_result:
+            self.logger.warning("Closing Tiago's gripper...")
+            self.tiago.execute(gripper_closing_result.trajectory, controllers=[])
+
 def main():
     rclpy.init()
-    logger = get_logger("moveit_py_pick")
-    tiago = MoveItPy(node_name="moveit_py_pick")
-    robot_model = tiago.get_robot_model()
-    tiago_arm = tiago.get_planning_component("arm")
-    logger.info("moveit_py_pick node successfully connected to Tiago!")
-
-    ###########################################################################
-    # Plan 1 - set goal state with RobotState object (using its joint states)
-    ###########################################################################
-    """
-    # Create a state from the current robot pose. Remember current state for later
-    robot_initial_state = RobotState(robot_model)
-
-    # Set another robot_state to default values = Tiago's arm extended
-    robot_state = RobotState(robot_model)
-    robot_state.set_to_default_values()
-    
-    # Set plan start state to current state
-    tiago_arm.set_start_state_to_current_state()
-
-    # Set goal state to the initialized robot state
-    logger.info("Set goal state to the initialized robot state")
-    tiago_arm.set_goal_state(robot_state=robot_state)
-    
-    # Plan trajectory to the goal state
-    plan_result = tiago_arm.plan()
-
-    # If it worked, execute the planned motion
-    if plan_result:
-        logger.info("Executing plan")
-        tiago.execute(plan_result.trajectory, controllers=[])
-    else:
-        logger.error("Planning failed")
-    
-    # Go back to the initial state saved before
-    tiago_arm.set_start_state_to_current_state()
-    tiago_arm.set_goal_state(robot_state=robot_initial_state)
-    plan_result = tiago_arm.plan()
-    if plan_result:
-        logger.warn("Moving arm to the joints goal (RobotState goal)...")
-        tiago.execute(plan_result.trajectory, controllers=[])
-    """
-    ###########################################################################
-    # Plan 2 - set cartesian goal for the gripper with PoseStamped message
-    ###########################################################################
-    """
-    # Set plan start state to current state
-    tiago_arm.set_start_state_to_current_state()
-
-    # Create pose goal with PoseStamped message
-    pose_goal = PoseStamped()
-
-    # The frame for the following cordinates...
-    pose_goal.header.frame_id = "base_link"
-
-    # ... the target end effector position...
-    pose_goal.pose.position.x = 0.7
-    pose_goal.pose.position.y = 0.1
-    pose_goal.pose.position.z = 0.4
-
-    # ... and the target end effector quaternion...
-    pose_goal.pose.orientation.x = 0.0
-    pose_goal.pose.orientation.y = 0.0
-    pose_goal.pose.orientation.z = 0.0
-    pose_goal.pose.orientation.w = 1.0
-    # ...you can get these coordinates from the current robot pose with `ros2 run tf2_ros tf2_echo base_link gripper_grasping_frame`
-
-    tiago_arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="gripper_grasping_frame")
-
-    # Plan to the cartesian goal
-    plan_result = tiago_arm.plan()
-
-    # If planning worked, execute the planned motion
-    if plan_result:
-        logger.warn("Moving arm to the cartesian goal...")
-        tiago.execute(plan_result.trajectory, controllers=[])
-    """
-
-    ###########################################################################
-    # Plan 3 - Open and close gripper
-    ###########################################################################
-    """
-    tiago_gripper = tiago.get_planning_component("gripper")
-
-    # Gripper opening
-    gripper_open = RobotState(robot_model)
-    gripper_open_joints = {"left_finger": 0.04, "right_finger": 0.04}
-    gripper_open.set_joint_group_positions("gripper", list(gripper_open_joints.values()))
-    tiago_gripper.set_goal_state(robot_state=gripper_open)
-
-    gripper_opening_result = tiago_gripper.plan()
-    if gripper_opening_result:
-        logger.warn("Opening Tiago's gripper...")
-        tiago.execute(gripper_opening_result.trajectory, controllers=[])
-
-    # Gripper closing
-    gripper_closed = RobotState(robot_model)
-    gripper_closed_joints = {"left_finger": 0.0, "right_finger": 0.0}
-    gripper_closed.set_joint_group_positions("gripper", list(gripper_closed_joints.values()))
-    tiago_gripper.set_goal_state(robot_state=gripper_closed)
-
-    gripper_closing_result = tiago_gripper.plan()
-    if gripper_closing_result:
-        logger.warn("Closing Tiago's gripper...")
-        tiago.execute(gripper_closing_result.trajectory, controllers=[])
-    """
+    node = MoveItExecutorNode()
+    # Uncomment one of the following function calls to execute the respective plan
+    node.plan1()
+    # node.plan2()
+    # node.plan3()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
